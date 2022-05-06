a8937b3a417e74f41d3259fb2c6c626c96cd0246
kim bo ra <a52447879@gmail.com>
2022-05-06 14:49:28 +0900

    ✨[feat] store 생성 및 example 코드 작성

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactDOM from "react-dom/client";
+
+import { store } from "./store/index";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
