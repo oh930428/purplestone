7cc96554d1874b1338b848fb990e4ab442a77fda
kim bo ra <a52447879@gmail.com>
2022-05-06 14:51:18 +0900

    ✨ [feat] store 생성 및 example 코드 작성 2

---
+++ src/store/api/exampleApi.ts
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IContact } from "../../models/example.model";
+
+export const contactsApi = createApi({
+  reducerPath: "contactsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  tagTypes: ["Contact"],
+  endpoints: (builder) => ({
+    fetchContacts: builder.query<IContact[], void>({
+      query: () => "/contacts",
+      providesTags: ["Contact"],
+    }),
+    fetchContactById: builder.query<IContact, string>({
+      query: (id) => `/contacts/${id}`,
+    }),
+    addContact: builder.mutation<{}, IContact>({
+      query: (contact) => ({
+        url: "/contacts",
+        method: "POST",
+        body: contact,
+      }),
+      invalidatesTags: ["Contact"],
+    }),
+    deleteContact: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/contacts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Contact"],
+    }),
+    upDateContact: builder.mutation<void, IContact>({
+      query: ({ id, ...rest }) => ({
+        url: `/contacts/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Contact"],
+    }),
+  }),
+});
+
+export const {
+  useFetchContactsQuery,
+  useFetchContactByIdQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+  useUpDateContactMutation,
+} = contactsApi;
---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsApi } from "./api/exampleApi";
+
+export const store = configureStore({
+  reducer: {
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(contactsApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
