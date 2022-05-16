80890739a3d5046e2643b8867c4bd87721122015
bora <a52447879@gmail.com>
2022-05-17 08:53:45 +0900

    ✨ [feat] Home, All Coffes, MyCard 컴포넌트 생성

---
+++ src/pages/AllCoffes/AllCoffes.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const AllCoffes = () => {
+  return <Container>AllCoffes</Container>;
+};
+
+export default AllCoffes;
+
+const Container = styled.div``;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Home = () => {
+  return <Container>Home</Container>;
+};
+
+export default Home;
+
+const Container = styled.div``;
