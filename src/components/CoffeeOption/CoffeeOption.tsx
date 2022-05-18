9585e6c45256fb2d600e2d5df450fd1b6416800c
bora <a52447879@gmail.com>
2022-05-19 08:09:30 +0900

    ✨ [feat] CoffeeOption 컴포넌트 추가 (선택시 색변경, 카드 이미지 선택시 이미지변경)

--- src/components/CoffeeOption/CoffeeOption.tsx
+++ src/components/CoffeeOption/CoffeeOption.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import circle from '../../assets/Images/bg-circle.png';
 import circleActive from '../../assets/Images/bg-circle-active.png';
+import ProcessCard from 'components/Card/ProcessCard/ProcessCard';
+
 import { useState } from 'react';
 import { CoffeeOptionProps } from 'types/myCard.types';
 
@@ -9,22 +11,50 @@ interface Props {
 }
 
 const CoffeeOption = ({ option }: Props) => {
-  const [isOn, setIsOn] = useState<boolean>(false);
+  const [isPopupCard, setIsPupupCard] = useState<boolean>(false);
+  const [selected, setSelected] = useState('');
 
-  const onClickToggle = () => setIsOn(!isOn);
+  const onClickActive = (e: any) => {
+    const target = e.currentTarget;
+    const optionItem = document.querySelectorAll('.option-box');
+    const newOptionItem = Array.from(optionItem);
+
+    for (let item of newOptionItem) {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      }
+    }
+
+    target.classList.add('active');
+    setIsPupupCard(prev => !prev);
+  };
 
   return (
     <Container
-      className={option.name}
+      className="option-box"
+      onClick={onClickActive}
       circle={circle}
       circleActive={circleActive}
-      isOn={isOn}
-      onClick={onClickToggle}
     >
-      <img
-        src={require(`../../assets/Icons/${option.image}`)}
-        alt={option.name}
-      />
+      {selected !== '' ? (
+        <img
+          className="option-image"
+          src={require(`../../assets/Icons/${selected}`)}
+          alt={option.name}
+        />
+      ) : (
+        <img
+          className="option-image"
+          src={require(`../../assets/Icons/${option.thumbnail}`)}
+          alt={option.name}
+        />
+      )}
+
+      {isPopupCard && (
+        <ProcessCardContainer className="process-card-container">
+          <ProcessCard option={option} setSelected={setSelected} />
+        </ProcessCardContainer>
+      )}
     </Container>
   );
 };
@@ -34,21 +64,38 @@ export default CoffeeOption;
 const Container = styled.article<{
   circle: string;
   circleActive: string;
-  isOn: boolean;
 }>`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 12rem;
   height: 12rem;
   cursor: pointer;
+  background-image: url(${circle});
+  background-size: cover;
+  background-repeat: no-repeat;
 
-  background-image: ${props =>
-    props.isOn ? `url(${props.circleActive})` : `url(${props.circle})`};
-
-  img {
+  .option-image {
     width: 9rem;
     height: 9rem;
     object-fit: contain;
   }
+
+  &.active {
+    background-image: url(${circleActive});
+
+    .process-card-container {
+      display: block;
+    }
+  }
+`;
+
+const ProcessCardContainer = styled.ul`
+  position: absolute;
+  left: 14rem;
+  top: 3rem;
+  width: 45rem;
+  z-index: 100;
+  display: none;
 `;
