39630431779c0fc0ed39e71b48533256a4110b1f
bora <a52447879@gmail.com>
2022-05-19 08:05:04 +0900

    ✨ [feat] MyCard 타입 및 db 수정

--- src/types/myCard.types.ts
+++ src/types/myCard.types.ts
@@ -2,27 +2,18 @@ export interface MyCardProps {
   title: string;
   subTitle: string;
   coffeeOption: CoffeeOptionProps[];
-  brand: MyCardContentsProps;
-  temperature: MyCardContentsProps;
-  beans: MyCardContentsProps;
-  coffee: MyCardContentsProps;
-  bottle: MyCardContentsProps;
 }
 
 export interface CoffeeOptionProps {
-  id: string;
   name: string;
-  image: string;
-}
-
-export interface MyCardContentsProps {
   text: string;
-  type: MyCardTypeProps[];
+  thumbnail: string;
+  types: MyCardTypeProps[];
 }
 
 export interface MyCardTypeProps {
   id: string;
   name: string;
   description: string;
-  thumbnail: string;
+  image: string;
 }
