e0e8352faf46cf942ea46780b747dcdb126871e4
oh min seok <oh930428@gmail.com>
2022-05-18 15:00:37 +0900

    ✨ [feat] OthersCoffee mock data 추가 및 otherscoffeeApi 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { creatorsApi } from './api/creators';
-import { contactsApi } from './api/exampleApi';
+import { otherscoffeeApi } from './api/otherscoffee';
 
 export const store = configureStore({
   reducer: {
-    [contactsApi.reducerPath]: contactsApi.reducer,
     [creatorsApi.reducerPath]: creatorsApi.reducer,
+    [otherscoffeeApi.reducerPath]: otherscoffeeApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(contactsApi.middleware),
+    getDefaultMiddleware().concat(creatorsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
