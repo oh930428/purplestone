c55463c2fb563d74f6d05acadbcf4c6b60c3c95a
bora <a52447879@gmail.com>
2022-05-18 11:10:41 +0900

    ✨ [feat] makeMyCard API 추가 및 리듀서 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { creatorsApi } from './api/creators';
 import { contactsApi } from './api/exampleApi';
+import { makeMyCardApi } from './api/makeMyCard';
 
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
     [creatorsApi.reducerPath]: creatorsApi.reducer,
+    [makeMyCardApi.reducerPath]: makeMyCardApi.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(contactsApi.middleware),
