1df3849ab74bcd5fa166c39a99221af0c843253a
bora <a52447879@gmail.com>
2022-05-25 15:06:45 +0900

    ✨ [feat] UserCardSmall 컴포넌트 추가

---
+++ src/components/Card/UserCardSmall/UserCardSmall.tsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import UserCardSmallOption from './UserCardSmallOption';
+import CardImage from '../../../assets/Images/bg-card-small.png';
+import IconsCoffeeOption from 'components/Icon/IconsCoffeeOption';
+
+import { colors } from 'styles';
+import { userCardProps } from '../../../types/userCardSmall';
+
+interface UserCardSmallProps {
+  card: userCardProps;
+}
+
+const UserCardSmall = ({ card }: UserCardSmallProps) => {
+  return (
+    <Container bgCard={CardImage}>
+      <Card>
+        <div className="header">
+          <div className="header-title">
+            <span>{card.userName}</span>님의 &nbsp;
+            <span>커피 취향</span>
+          </div>
+          <div className="header-section">
+            {Object.keys(card.userCardSmallType).map((key, index) => (
+              <IconsCoffeeOption
+                key={index}
+                option={card.userCardSmallType[key]}
+              />
+            ))}
+          </div>
+        </div>
+        <UserCardSmallOption option={card.userCardSmallType} />
+      </Card>
+    </Container>
+  );
+};
+
+export default UserCardSmall;
+
+const Container = styled.div<{ bgCard: string }>`
+  width: calc(100% / 3 - (3rem));
+  min-width: 30rem;
+  height: 46rem;
+  background-image: ${props => `url(${props.bgCard})`};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 20px;
+  box-sizing: border-box;
+  padding: 3rem 2.5rem;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.25);
+`;
+
+const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  gap: 7rem;
+
+  .header {
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
+
+    .header-title {
+      text-align: center;
+      letter-spacing: -0.01em;
+      line-height: 32px;
+      color: #614e32;
+      font-family: 'Noto Sans';
+      font-weight: 400;
+      font-size: 1.8rem;
+      line-height: 25px;
+
+      span {
+        font-size: 2.4rem;
+        font-weight: 700;
+        color: ${colors.Gray_01};
+        background: linear-gradient(to top, #f5f2c1 50%, transparent 50%);
+      }
+    }
+
+    .header-section {
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      grid-auto-columns: minmax(48px, auto);
+      grid-gap: 5px;
+    }
+  }
+`;
---
+++ src/components/Card/UserCardSmall/UserCardSmallOption.tsx
@@ -0,0 +1,108 @@
+import { fonts } from 'styles';
+import styled from 'styled-components';
+import DigramImage from '../../../assets/Images/bg-digram-small.png';
+import { userCardSmallTypeProps } from '../../../types/userCardSmall';
+
+interface Props {
+  option: userCardSmallTypeProps;
+}
+
+const UserCardSmallOption = ({ option }: Props) => {
+  return (
+    <Container>
+      <Digram bgDigram={DigramImage}>
+        <div className="thumbnail">
+          <img
+            src={require(`../../../assets/Icons/${option.coffeeType.image}`)}
+            alt="다이어그램 이미지"
+          />
+        </div>
+        <ul>
+          {Object.keys(option).map((key, index) => (
+            <li className={option[key].type} key={index}>
+              <span>{option[key].name}</span>
+            </li>
+          ))}
+        </ul>
+      </Digram>
+    </Container>
+  );
+};
+
+export default UserCardSmallOption;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Digram = styled.figure<{ bgDigram: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 17rem;
+  background-image: ${props => `url(${props.bgDigram})`};
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+
+  .thumbnail {
+    display: inline-flex;
+    width: 9rem;
+    height: 10rem;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  ul {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    li {
+      position: absolute;
+      box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
+      width: 12rem;
+      padding: 0.2rem 1rem;
+      ${fonts.BoldBody1}
+      font-size: 1.4rem;
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 10px;
+      text-align: center;
+      word-break: keep-all;
+
+      &.brand {
+        left: 31%;
+        top: -19%;
+
+        img {
+          width: 95%;
+          height: 95%;
+        }
+      }
+      &.beans {
+        right: -5%;
+        top: 25%;
+      }
+      &.bottle {
+        right: 5%;
+        bottom: -16%;
+      }
+      &.coffeeType {
+        left: 5%;
+        bottom: -16%;
+      }
+      &.temperature {
+        left: -5%;
+        top: 25%;
+      }
+    }
+  }
+`;
