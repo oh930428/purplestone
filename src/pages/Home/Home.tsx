3357aa5a1ab36101f274b7bb73d459d0e67f5207
oh min seok <oh930428@gmail.com>
2022-06-03 03:14:32 +0900

    ✨ [feat] Home page MakeCard, OtherCoffee 세션 반응형 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,67 @@
 import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
+
 import { HeaderSection, Creators, OthersCoffee, MakeCard } from './components';
+import { desktopMain, mobileMain, tabletMain } from 'styles/mixin';
 
 const Home = () => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 1180px)' });
+  const isTablet = useMediaQuery({
+    query: '(min-width: 540px) and (max-width: 1179px)',
+  });
+  const isMobile = useMediaQuery({ query: '(max-width: 539px)' });
   return (
     <Container>
       <HeaderSection />
-      <OthersCoffee />
-      <MakeCard />
+      {isDesktop && (
+        <>
+          <OthersCoffee
+            maxWidth={desktopMain.maxWidth}
+            margin={desktopMain.margin}
+            font={desktopMain.font}
+            subFont={desktopMain.subFont}
+          />
+          <MakeCard
+            maxWidth={desktopMain.maxWidth}
+            margin={desktopMain.margin}
+            font={desktopMain.font}
+            subFont={desktopMain.subFont}
+          />
+        </>
+      )}
+      {isTablet && (
+        <>
+          <OthersCoffee
+            maxWidth={tabletMain.maxWidth}
+            margin={tabletMain.margin}
+            font={tabletMain.font}
+            subFont={tabletMain.subFont}
+          />
+          <MakeCard
+            maxWidth={tabletMain.maxWidth}
+            margin={tabletMain.margin}
+            font={tabletMain.font}
+            subFont={tabletMain.subFont}
+          />
+        </>
+      )}
+      {isMobile && (
+        <>
+          <OthersCoffee
+            maxWidth={mobileMain.maxWidth}
+            margin={mobileMain.margin}
+            font={mobileMain.font}
+            subFont={mobileMain.subFont}
+          />
+          <MakeCard
+            maxWidth={mobileMain.maxWidth}
+            margin={mobileMain.margin}
+            font={mobileMain.font}
+            subFont={mobileMain.subFont}
+          />
+        </>
+      )}
+
       <Creators />
     </Container>
   );
