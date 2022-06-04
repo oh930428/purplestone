ee7f1c7b3a5c114554a9ba8d85b172138b176d43
bora <a52447879@gmail.com>
2022-06-04 20:34:23 +0900

    ✨ [feat] Navigation 반응형 추가

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,35 +1,48 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Button from '../Button/Button';
 
 import { colors } from '../../styles';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { desktopMain, mobileMain, tabletMain } from 'styles/mixin';
+import { ResponsiveByDeivceProps } from 'types/responsiveByDevice.type';
 
 const Navigation = () => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 1180px)' });
+  const isTablet = useMediaQuery({
+    query: '(min-width: 768px) and (max-width: 1179px)',
+  });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
   return (
-    <Container>
+    <Container isDesktop={isDesktop}>
       <div className="wrapper">
         <Logo>
           <Link to="/">PurpleStone</Link>
         </Logo>
 
-        <Gnb>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/all-coffees">All Coffees</Link>
-          </li>
-          <li className="nav-button-item">
-            <Link to="/mycard">
-              <Button
-                size="medium"
-                theme="primary"
-                label="Create My Card"
-                isRegular
-              ></Button>
-            </Link>
-          </li>
-        </Gnb>
+        {isDesktop && (
+          <Gnb>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/all-coffees">All Coffees</Link>
+            </li>
+            <li className="nav-button-item">
+              <Link to="/mycard">
+                <Button
+                  size="medium"
+                  theme="primary"
+                  label="Create My Card"
+                  isRegular
+                ></Button>
+              </Link>
+            </li>
+          </Gnb>
+        )}
+
+        {!isDesktop && <div>햄버거</div>}
       </div>
     </Container>
   );
@@ -37,18 +50,31 @@ const Navigation = () => {
 
 export default Navigation;
 
-const Container = styled.nav`
+const Container = styled.nav<{ isDesktop: boolean }>`
   position: absolute;
   top: 20px;
   width: 100%;
+  padding: 0 2rem;
   z-index: 100;
+
+  ${props =>
+    props.isDesktop &&
+    css`
+      padding: 0;
+    `}
 
   .wrapper {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    max-width: 1400px;
+    width: 100%;
     margin: 0 auto;
+
+    ${props =>
+      props.isDesktop &&
+      css`
+        max-width: 1400px;
+      `}
   }
 
   a {
