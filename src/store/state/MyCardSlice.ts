894f7bea75e7d07cadd487282375b7b1f388db40
kimbora <a52447879@gmail.com>
2022-06-08 20:26:39 +0900

    ✨ [feat] properties 폴더 생성, MyCardSlice 초기 데이터(initialState)를 properties폴더에서 관리

--- src/store/state/MyCardSlice.ts
+++ src/store/state/MyCardSlice.ts
@@ -1,47 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { UserCardProps, CoffeeTypeProps } from '../../types/createMyCard';
-
-const userMyCard: UserCardProps = {
-  brand: {
-    id: 1,
-    name: 'EDIYA COFFEE',
-    description: '이디아 브랜드가 좋아요',
-    image: 'brands/ediya.png',
-    type: 'brand',
-  },
-  beans: {
-    id: 2,
-    name: '콜롬비아 수프리모',
-    description: '적절한 산미가 깔끔한 맛의 콜롬비아 수프리모가 내 취향~',
-    image: 'beans/colombiaSupremo.png',
-    type: 'beans',
-  },
-  coffeeType: {
-    id: 3,
-    name: '아메리카노',
-    description: '내 마음에 아메리카노',
-    image: 'coffeetypes/americano.png',
-    type: 'coffeeType',
-  },
-  bottle: {
-    id: 4,
-    name: '텀블러',
-    description: '텀블러',
-    image: 'bottles/cup-tumbler.png',
-    type: 'bottle',
-  },
-  temperature: {
-    id: 5,
-    name: '쪄.죽.따',
-    description: '이열치열! 여름에도 뜨거운 커피 못 잃어',
-    image: 'temperature/hot.png',
-    type: 'temperature',
-  },
-};
+import { coffeeDefaultOption } from 'properties/coffeDefaultOption';
+import { CoffeeTypeProps } from '../../types/createMyCard';
 
 export const myCardSlice = createSlice({
   name: 'coffeeOption',
-  initialState: userMyCard,
+  initialState: coffeeDefaultOption,
   reducers: {
     setSelectOptions: (state, action: PayloadAction<CoffeeTypeProps>) => {
       const updated = { ...state };
