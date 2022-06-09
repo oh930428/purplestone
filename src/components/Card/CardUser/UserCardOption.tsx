5ac1c3a7dd009b2eba336d38deabd8a943e713f7
bora <a52447879@gmail.com>
2022-06-10 00:12:29 +0900

    💄 [design] UserCardOption 이미지, 텍스트 배치 수정

---
+++ src/components/Card/CardUser/UserCardOption.tsx
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+import { fonts, colors } from 'styles';
+import { CoffeeType } from '../../../types/createMyCard.type';
+interface Props {
+  option: CoffeeType;
+}
+
+const UserCardOption = ({ option }: Props) => {
+  return (
+    <Container className={`type-box ${option.type}`}>
+      <span className="name">{option.name}</span>
+      <p className="description">{option.description}</p>
+      <ImageContainer className={`${option.type}-image`}>
+        <img
+          src={require(`../../../assets/Icons/${option.image}`)}
+          alt={option.type}
+        />
+      </ImageContainer>
+    </Container>
+  );
+};
+
+export default UserCardOption;
+
+const Container = styled.article`
+  position: absolute;
+
+  &.bottle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    left: 34.5%;
+    top: -15%;
+    width: 25rem;
+
+    .name {
+      margin-right: 1rem;
+    }
+  }
+
+  &.beans {
+    right: 9%;
+    top: 30%;
+    width: 15rem;
+  }
+
+  &.coffeeType {
+    bottom: -20%;
+    right: 20%;
+    width: 15rem;
+  }
+
+  &.brand {
+    bottom: -5rem;
+    left: 21rem;
+  }
+
+  &.temperature {
+    left: 9%;
+    top: 30%;
+    width: 15rem;
+    text-align: right;
+  }
+
+  .name {
+    ${fonts.Headline4}
+    color: #614E32;
+    word-break: keep-all;
+  }
+
+  .description {
+    ${fonts.MediumCaption};
+    color: ${colors.Gray_02};
+    word-break: keep-all;
+  }
+`;
+
+const ImageContainer = styled.figure`
+  position: absolute;
+  width: 5rem;
+  height: 5rem;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &.bottle-image {
+    width: 11rem;
+    height: 11rem;
+    top: 400%;
+    left: 24%;
+    z-index: 100;
+  }
+
+  &.beans-image {
+    top: -5rem;
+  }
+
+  &.coffeeType-image {
+    left: 10rem;
+    top: -0.5rem;
+  }
+
+  &.brand-image {
+    left: -6rem;
+    top: -0.5rem;
+  }
+
+  &.temperature-image {
+    top: -100%;
+    right: -30%;
+  }
+`;
